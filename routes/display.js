fdd88b84f75eddaf9b4616ae2bb268da8d0347ff
NEDuck <cj96cj9696cj@gmail.com>
2023-03-27 21:22:51 +0800

    Solve name matching problem

--- routes/display.js
+++ routes/display.js
@@ -7,14 +7,15 @@ router.get('/', async (req, res) => {
     const name = req.query.name;
     const imgArray = Object.entries(assetsIndex)
         .filter(([_, {title: fileName}]) => fileName.match(/\.(png|jpg|jpeg)$/i))
-        .filter(([_, {title: fileName}]) => fileName.includes(name))
+        .filter(([_, {title: fileName}]) => fileName.match(new RegExp(`(?:(?:^.*(?:詹|\\P{Script=Han})|^))${name}(?:\\s|\\.).*`, 'u')))
         .map(([fileId]) => fileId);
     const docxArray = Object.entries(assetsIndex)
         .filter(([_, {title: fileName}]) => fileName.match(/\.(doc|docx)$/i))
-        .filter(([_, {title: fileName}]) => fileName.includes(name))
+        .filter(([_, {title: fileName}]) => fileName.match(new RegExp(`(?:(?:^.*(?:詹|\\P{Script=Han})|^))${name}(?:\\s|\\.).*`, 'u')))
         .map(([fileId]) => fileId);
 
     res.render('display', { docxArray, imgArray });
 })
 
 export default router
+// \P{Script=Han}
