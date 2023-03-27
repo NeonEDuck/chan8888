fcc6d97de89fd3d74bdc0b49a745bccbc630a992
NEDuck <cj96cj9696cj@gmail.com>
2023-03-28 00:06:11 +0800

    Use altName for searching image

--- routes/display.js
+++ routes/display.js
@@ -4,18 +4,21 @@ import { assetsIndex } from './assets.js';
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const name = req.query.name;
+    const nameArray = req.query.name.split(',');
     const imgArray = Object.entries(assetsIndex)
         .filter(([_, {title: fileName}]) => fileName.match(/\.(png|jpg|jpeg)$/i))
-        .filter(([_, {title: fileName}]) => fileName.match(new RegExp(`(?:(?:^.*(?:詹|\\P{Script=Han})|^))${name}(?:\\s|\\.).*`, 'u')))
+        .filter(([_, {title: fileName}]) => {
+            return nameArray.some((name) => fileName.match(new RegExp(`(?:(?:^.*(?:詹|\\P{Script=Han})|^))${name}\\P{Script=Han}.*`, 'u')))
+        })
         .map(([fileId]) => fileId);
     const docxArray = Object.entries(assetsIndex)
         .filter(([_, {title: fileName}]) => fileName.match(/\.(doc|docx)$/i))
-        .filter(([_, {title: fileName}]) => fileName.match(new RegExp(`(?:(?:^.*(?:詹|\\P{Script=Han})|^))${name}(?:\\s|\\.).*`, 'u')))
+        .filter(([_, {title: fileName}]) => {
+            return nameArray.some((name) => fileName.match(new RegExp(`(?:(?:^.*(?:詹|\\P{Script=Han})|^))${name}\\P{Script=Han}.*`, 'u')))
+        })
         .map(([fileId]) => fileId);
 
     res.render('display', { docxArray, imgArray });
 })
 
 export default router
-// \P{Script=Han}
